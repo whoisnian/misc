84efb17ea5399578351689a630fe813ad1f0e90e
whoisnian <zhuchangbao1998@gmail.com>
2025-09-24 02:37:57 +0800

    ADD: cmd/term fe eslint config

---
+++ cmd/term/fe/eslint.config.js
@@ -0,0 +1,17 @@
+import neostandard, { resolveIgnoresFromGitignore } from 'neostandard'
+import globals from 'globals'
+
+export default [
+  ...neostandard({
+    env: ['browser'],
+    globals: {
+      __DEBUG__: 'readonly',
+      __PACKAGE_VERSION__: 'readonly'
+    },
+    files: ['src/**/*.js', 'scripts/**/*.js'],
+    ignores: [...resolveIgnoresFromGitignore(), 'dist/**/*']
+  }),
+  {
+    languageOptions: { ecmaVersion: 2025, globals: globals.es2025 }
+  }
+]
